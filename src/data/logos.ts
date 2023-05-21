2d8b457ee4ea2ebd86a129719531b29463991757
Khalil Melhem <khalil-melhem@outlook.sa>
2023-05-21 21:07:00 +0300

    Game Camp 1.0.0 🚀

---
+++ src/data/logos.ts
@@ -0,0 +1,35 @@
+export const logos = [
+  {
+    name: "Al-Yamamah Univirsity Logo",
+    src: "/images/logos/Yu.png",
+    href: "https://yu.edu.sa/",
+  },
+  {
+    name: "Tuwaiq Academy Logo",
+    src: "/images/logos/TuwaiqAcademy.png",
+    href: "https://tuwaiq.edu.sa/",
+  },
+  {
+    name: "GDSC Al-Yamamah Univirsity Logo",
+    src: "/images/logos/gdsc.png",
+    href: "https://gdscyu.com/",
+    bright: true,
+  },
+  {
+    name: "Player One Logo",
+    src: "/images/logos/PlayerOne.png",
+    href: "https://playeronegames.net/",
+    bright: true,
+  },
+  {
+    name: "Digital Game Association Logo",
+    src: "/images/logos/dga.png",
+    href: "https://dga.sa/",
+    bright: true,
+  },
+  {
+    name: "SaudiGN Logo",
+    src: "/images/logos/SaudiGN.png",
+    href: "https://www.saudigamenews.com/en/",
+  },
+];
