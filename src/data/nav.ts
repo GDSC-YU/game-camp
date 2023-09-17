14e32b5439e5db9a29df7f16a5aecb1d339fa783
Khalil Melhem <me@khalil.im>
2023-09-18 00:27:14 +0300

    🔧 refactor(website): revamped the website

--- src/data/nav.ts
+++
@@ -1,7 +0,0 @@
-export const navData = [
-  { name: "Home 👾", href: "/" },
-  { name: "Speakers 💬", href: "#speakers" },
-  { name: "Judges 🧑‍⚖️ &\n Consultants 🧑‍💻", href: "#people" },
-  { name: "Schedule  🔧", href: "#Schedule" },
-  { name: "FAQ 🤔", href: "#faq" },
-];
