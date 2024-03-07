183344c9411402461eade94c94e8389c2be43755
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-03-08 01:54:34 +0300

    pump dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
-import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://game-camp.gdscyu.com",
-  integrations: [tailwind(), prefetch(), sitemap()],
+  integrations: [tailwind(), sitemap(), icon()],
 });
