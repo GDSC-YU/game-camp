4163e60c9179b9024e79524388d7245a706f23e0
RawanMortada <ramo21sash@gmail.com>
2024-03-27 17:49:30 +0300

    added a react file for the carousel and a corresponding css file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import react from '@astrojs/react';
 
 import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://game-camp.gdscyu.com",
-  integrations: [tailwind(), sitemap(), icon()],
+  integrations: [tailwind(), sitemap(), icon(), react()],
 });
