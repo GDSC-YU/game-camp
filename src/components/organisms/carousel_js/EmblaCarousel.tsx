e6a001d41bab4005f83830e98dbb2416d3be0b91
RawanMortada <ramo21sash@gmail.com>
2024-04-07 23:15:10 +0300

    did some changes based on the feedback

--- src/components/organisms/carousel_js/EmblaCarousel.tsx
+++ src/components/organisms/carousel_js/EmblaCarousel.tsx
@@ -1,32 +1,29 @@
-import React, { useEffect } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-import '../carousel_js/embla.css'
-
-import background1 from 'src/assets/images/backgrounds/1.gif'
-import background2 from 'src/assets/images/backgrounds/2.gif'
-import background3 from 'src/assets/images/backgrounds/3.gif'
+import React, { useRef } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import background1 from '../../../assets/images/backgrounds/1.gif';
+import background2 from '../../../assets/images/backgrounds/2.gif';
+import background3 from '../../../assets/images/backgrounds/3.gif';
 
 
-export function EmblaCarousel() {
-  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
-  const placeholderImages =["src/assets/images/backgrounds/1.gif", "src/assets/images/backgrounds/2.gif", "src/assets/images/backgrounds/3.gif"]
+const EmblaCarousel: React.FC = () => {
+
+  const [emblaRef] = useEmblaCarousel();
+
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        {placeholderImages.map((src, index) => (
-          <div className="embla__slide" key ={index}>
-            <img
-            src ={src}
-            className="embla__image"
-            alt = {`Slide ${index + 1}`}
-            />
-            </div>
-        ))}
-    </div> 
+    <div className="embla" ref={emblaRef} style={{overflow: 'hidden'}}>
+      <div className="embla--container">
+        <div className="embla__slide ">
+          <img src= '../../../assets/images/backgrounds/1.gif' alt='Slide1'/>
+        </div>
+        <div className="embla__slide ">
+          <img src='../../../assets/images/backgrounds/2.gif' alt='Slide2'/>
+        </div>
+        <div className="embla__slide ">
+          <img src='../../../assets/images/backgrounds/3.gif' alt='Slide3'/>
+          </div>
+      </div>
     </div>
   )
-  }
-  
+};
 
-export default EmblaCarousel
+export default EmblaCarousel;
